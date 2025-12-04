57c2444c0d267a94fb9a533fea19ee14e0e17d51
NoahKrobot <noah@certus-sustavi.hr>
2025-12-04 19:29:44 +0000

    Add first DTO

---
+++ GarrysMod/DTOs/DTO_GarrysItem.cs
@@ -0,0 +1,13 @@
+﻿namespace GarrysMod.DTOs
+{
+    public class DTO_GarrysItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CreatorUserName { get; set; }
+        public int CreatorID { get; set; }
+        public string MapName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
